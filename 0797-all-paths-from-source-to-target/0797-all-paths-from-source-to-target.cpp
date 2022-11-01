d35d2e19c3115a2d9c8e7b48008275c9221244a1
Kaab <42168364+Kaabislam@users.noreply.github.com>
2022-11-02 01:47:46 +0600

    Time: 23 ms (66.08%), Space: 12.1 MB (42.33%) - LeetHub

--- 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.cpp
+++ 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.cpp
@@ -4,15 +4,8 @@ public:
     vector<int>v;
     vector<vector<int>>ans;
     void rec(int cur,vector<vector<int>>& graph){
-        //cout << cur << " ";
         if(cur == dest){
             ans.push_back(v);
-            // for(int i = 0;i < v.size();i++){
-            //     cout << v[i] << " ";
-            // }
-            // cout << endl;
-            //cout << endl;
-            //cout << dest << endl;
             return;
         }
         for(int i = 0;i < graph[cur].size();i++){
@@ -31,12 +24,10 @@ public:
     vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) {
         src = 0;
         dest = graph.size()-1;
-       // cout << graph[0].size() << endl;
-        //for(int i = 0;i < graph[0].size();i++){
-            v.push_back(0);
-            rec(0,graph);
-        //}
-    
+        v.push_back(0);
+        rec(0,graph);
+        
+
         return ans;
     }
 };
