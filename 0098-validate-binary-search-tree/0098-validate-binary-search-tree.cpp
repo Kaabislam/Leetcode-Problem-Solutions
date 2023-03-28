a7400afaac51f24f07425ef7ec4edc13f9c8a8c0
Kaab <42168364+Kaabislam@users.noreply.github.com>
2023-03-29 03:17:09 +0600

    Time: 17 ms (34.07%), Space: 22.1 MB (13.75%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
@@ -12,23 +12,19 @@
 class Solution {
 public:
     bool isValidBST(TreeNode* root) {
-        
         queue<pair<long long,long long>>q1;
         queue<TreeNode*>q2;
+        
         q2.push(root);
         q1.push({LLONG_MIN,LLONG_MAX});
+        
         while(!q2.empty()){
             pair<long long,long long>x = q1.front();
             TreeNode *y = q2.front();
-            // cout << y->val << " " << x.first << " " << x.second << endl;
             if(y->val <= x.first or y->val >= x.second){
-                
-                // cout << y->val << endl;
                 return false;
             }
-            
             if(y->left){
-                
                 q2.push(y->left);
                 q1.push({x.first,y->val});
             }
@@ -36,10 +32,6 @@ public:
                 q2.push(y->right);
                 q1.push({y->val,x.second});
             }
-            
-            
-            
-            
             q1.pop();
             q2.pop();
         }
