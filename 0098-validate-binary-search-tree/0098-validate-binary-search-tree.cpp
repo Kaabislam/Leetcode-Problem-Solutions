b478a537d9cd90afbf96e7c688c2d1b9a8121425
Kaab <42168364+Kaabislam@users.noreply.github.com>
2023-03-29 03:15:58 +0600

    Time: 7 ms (95.98%), Space: 22.2 MB (13.75%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
@@ -11,37 +11,38 @@
  */
 class Solution {
 public:
-    bool check(TreeNode* root){
-        queue<TreeNode*>q;
-        if(root->left) q.push(root->left);
-        while(!q.empty()){
-            TreeNode* x = q.front();
-            if(x->val >= root->val) return false;
-            if(x->left) q.push(x->left);
-            if(x->right) q.push(x->right);
-            q.pop();
-        }
-        if(root->right) q.push(root->right);
-        while(!q.empty()){
-            TreeNode* x = q.front();
-            if(x->val <= root->val) return false;
-            if(x->left) q.push(x->left);
-            if(x->right) q.push(x->right);
-            q.pop();
-        }  
-        return true;
-        
-    }
     bool isValidBST(TreeNode* root) {
-        queue<TreeNode*>q;
-        q.push(root);
-        while(!q.empty()){
-            TreeNode* x = q.front();
-            if(!check(x)){return false;}
-            if(x->left) q.push(x->left);
-            if(x->right) q.push(x->right);
-            q.pop();
-        }  
+        
+        queue<pair<long long,long long>>q1;
+        queue<TreeNode*>q2;
+        q2.push(root);
+        q1.push({LLONG_MIN,LLONG_MAX});
+        while(!q2.empty()){
+            pair<long long,long long>x = q1.front();
+            TreeNode *y = q2.front();
+            // cout << y->val << " " << x.first << " " << x.second << endl;
+            if(y->val <= x.first or y->val >= x.second){
+                
+                // cout << y->val << endl;
+                return false;
+            }
+            
+            if(y->left){
+                
+                q2.push(y->left);
+                q1.push({x.first,y->val});
+            }
+            if(y->right){
+                q2.push(y->right);
+                q1.push({y->val,x.second});
+            }
+            
+            
+            
+            
+            q1.pop();
+            q2.pop();
+        }
         return true;
     }
 };
